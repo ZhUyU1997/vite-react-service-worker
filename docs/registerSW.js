99d8da75ee587edcdd68713abd7c1b6ede231b04
yzhu <891085309@qq.com>
2022-03-09 22:46:17 +0800

    [fix] base path

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-react-service-worker/sw.js', { scope: '/vite-react-service-worker/' })})}
